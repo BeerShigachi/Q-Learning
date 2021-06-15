aa7ea79eb1b869867e56600f50af6098401cfee0
beer_shigachi <beer.shigach@gmail.com>
2021-06-15 18:55:17 +0900

    Added epsilon and decaying.

--- q_learning.py
+++ q_learning.py
@@ -1,20 +1,21 @@
 import gym
 import numpy as np
-import pprint
 
 env = gym.make("MountainCar-v0")
-observation = env.reset()
 
 LEARNING_ALPHA = 0.1
 DISCOUNT = 0.95
 DISCRETE_OBS_SIZE = [20] * len(env.observation_space.high)
-EPISODES = 100000
-
-SHOW_RATE = 100
-
+EPISODES = 5000
+SHOW_RATE = 200
 discreate_obs_window_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OBS_SIZE
 
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OBS_SIZE + [env.action_space.n]))
+
+epsilon = 0.5
+EPSILON_DECAY_START = 1  # probably much higher starting episodes
+EPSILON_DECAY_END = EPISODES // 2
+epsilon_decay_val = epsilon / (EPSILON_DECAY_END - EPSILON_DECAY_START)
 
 
 def get_discrete_state(obs):
@@ -22,31 +23,39 @@ def get_discrete_state(obs):
     return tuple(res.astype(np.int_))
 
 
-discrete_observation = get_discrete_state(observation)
-
 for ep in range(EPISODES):
 
+    render = False
     if ep % SHOW_RATE == 0:
         print(ep)
-        env.render()
+        render = True
 
-    action = np.argmax(q_table[discrete_observation])  # agent here
-    observation, reward, done, info = env.step(action)
-    new_observation = get_discrete_state(observation)
-    if done:
-        observation = env.reset()
-        if new_observation[0] >= env.goal_position:
+    discrete_observation = get_discrete_state(env.reset())
+    done = False
+
+    while not done:
+        action = np.random.randint(0, env.action_space.n)
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[discrete_observation])  # agent here
+        observation, reward, done, info = env.step(action)
+        new_observation = get_discrete_state(observation)
+        if render:
+            env.render()
+        if not done:
+            max_next_q_val = np.max(q_table[new_observation])
+            current_q_val = q_table[discrete_observation + (action,)]
+
+            new_q_val = (1 - LEARNING_ALPHA) * current_q_val + LEARNING_ALPHA * (reward + DISCOUNT * max_next_q_val)
+
+            q_table[discrete_observation + (action,)] = new_q_val
+
+        elif observation[0] >= env.goal_position:
+            print(f"done! on ep{ep}", discrete_observation, action)
             q_table[discrete_observation + (action,)] = 0
-            continue
-        else:
-            print(f"done on episode {ep}!")
-            break
-    max_next_q_val = np.max(q_table[new_observation])
-    current_q_val = q_table[discrete_observation + (action,)]
 
-    new_q_val = (1 - LEARNING_ALPHA) * current_q_val + LEARNING_ALPHA * (reward + DISCOUNT * max_next_q_val)
-    q_table[discrete_observation + (action,)] = new_q_val
+        discrete_observation = new_observation
 
-    discrete_observation = new_observation
+    if EPSILON_DECAY_END >= ep >= EPSILON_DECAY_START:
+        epsilon -= epsilon_decay_val
 
 env.close()
